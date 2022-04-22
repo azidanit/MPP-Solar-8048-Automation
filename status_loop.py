5bfff5d8a9e683760b0a6ef766e89fcd98edf719
Achmad Zidan Akbar <52484970+azidanit@users.noreply.github.com>
2022-04-22 10:29:44 +0700

    Update status_loop.py

--- status_loop.py
+++ status_loop.py
@@ -5,5 +5,7 @@ while True:
     os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
     time.sleep(1)
     os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS2 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
+    time.sleep(1)
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QDI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
     time.sleep(30)
 
