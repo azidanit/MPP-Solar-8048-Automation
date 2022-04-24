4083ced7dea0aaad73862a9c224392b37b549782
azidanit <azidan.it@gmail.com>
2022-04-24 20:58:45 +0700

    add speed status

--- status_loop.py
+++ status_loop.py
@@ -8,13 +8,13 @@ while True:
     ############## 8048 #############
     print("PUSH 8048")
     os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(1)
+    time.sleep(0.5)
     os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS2 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(1)
+    time.sleep(0.5)
     os.system("mpp-solar -p /dev/ttyUSB0 -c QMOD -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(1)
+    time.sleep(0.5)
     os.system("mpp-solar -p /dev/ttyUSB0 -c Q1 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30 -o mqtt")
-    time.sleep(1)
+    time.sleep(0.5)
     print("PUSH 8048 done")
     ############## 8048 #############
 
@@ -51,26 +51,26 @@ while True:
         print("PUSH Another")
 
         os.system("mpp-solar -p /dev/ttyUSB0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system("mpp-solar -p /dev/ttyUSB0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4_conf -P PI30 -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
 
     
         os.system("mpp-solar -p /dev/hidraw0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1 -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system("mpp-solar -p /dev/hidraw0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1_conf -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
 
         os.system("mpp-solar -p /dev/hidraw1 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2 -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
         os.system("mpp-solar -p /dev/hidraw1 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2_conf -o mqtt")
-        time.sleep(1)
+        time.sleep(0.5)
         print("PUSH Another Done")
 
         counter_another_status = 0
     else:
         counter_another_status += 1
 
-    print("SLEEP 20s")
-    time.sleep(20)
+    # print("SLEEP 20s")
+    # time.sleep(20)
 
