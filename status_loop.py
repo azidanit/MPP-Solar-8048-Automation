06702ca8dbbf53c300e2ac2b2e032a93c9531fba
azidanit <azidan.it@gmail.com>
2022-04-24 21:18:24 +0700

    to multi thread

--- status_loop.py
+++ status_loop.py
@@ -2,22 +2,11 @@ from itertools import count
 import os
 import time
 
+from threading import Thread
+
 counter_another_status = 0
 
-while True:
-    ############## 8048 #############
-    print("PUSH 8048")
-    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(0.5)
-    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS2 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(0.5)
-    os.system("mpp-solar -p /dev/ttyUSB0 -c QMOD -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-    time.sleep(0.5)
-    os.system("mpp-solar -p /dev/ttyUSB0 -c Q1 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30 -o mqtt")
-    time.sleep(0.5)
-    print("PUSH 8048 done")
-    ############## 8048 #############
-
+def inverter1_status():
     ############## 5048 1 #############
     print("PUSH 5048 1")
     os.system("mpp-solar -p /dev/hidraw0 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1 -o mqtt")
@@ -30,8 +19,14 @@ while True:
     time.sleep(0.5)
     print("PUSH 5048 1 Done")
 
+    os.system("mpp-solar -p /dev/hidraw0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1 -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/hidraw0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1_conf -o mqtt")
+    time.sleep(0.5)
+
     ############## 5048 1 #############
 
+def inverter2_status():
     ############## 5048 2 #############
     print("PUSH 5048 1")
     os.system("mpp-solar -p /dev/hidraw1 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2 -o mqtt")
@@ -43,34 +38,63 @@ while True:
     os.system("mpp-solar -p /dev/hidraw1 -c QMOD -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2 -o mqtt")
     time.sleep(0.5)
     print("PUSH 5048 2 Done")
-
-
     ############## 5048 2 #############
 
-    if counter_another_status >= 1:
-        print("PUSH Another")
+    os.system("mpp-solar -p /dev/hidraw1 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2 -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/hidraw1 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2_conf -o mqtt")
+    time.sleep(0.5)
 
-        os.system("mpp-solar -p /dev/ttyUSB0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
-        time.sleep(0.5)
-        os.system("mpp-solar -p /dev/ttyUSB0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4_conf -P PI30 -o mqtt")
-        time.sleep(0.5)
+
+def inverter3_status():
+    ############## 8048 #############
+    print("PUSH 8048")
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIGS2 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QMOD -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/ttyUSB0 -c Q1 -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30 -o mqtt")
+    time.sleep(0.5)
+    print("PUSH 8048 done")
+    ############## 8048 #############
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4 -P PI30MAX -o mqtt")
+    time.sleep(0.5)
+    os.system("mpp-solar -p /dev/ttyUSB0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver4_conf -P PI30 -o mqtt")
+    time.sleep(0.5)
+
+
+t1_status = Thread(target=inverter1_status)
+t2_status = Thread(target=inverter2_status)
+t3_status = Thread(target=inverter3_status)
+
+# start the threads
+t1_status.start()
+t2_status.start()
+t3_status.start()
+
+# wait for the threads to complete
+t1_status.join()
+t2_status.join()
+t3_status.join()
+
+# while True:
+
+#     if counter_another_status >= 1:
+#         print("PUSH Another")
+
+        
 
     
-        os.system("mpp-solar -p /dev/hidraw0 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1 -o mqtt")
-        time.sleep(0.5)
-        os.system("mpp-solar -p /dev/hidraw0 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver1_conf -o mqtt")
-        time.sleep(0.5)
 
-        os.system("mpp-solar -p /dev/hidraw1 -c QPIWS -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2 -o mqtt")
-        time.sleep(0.5)
-        os.system("mpp-solar -p /dev/hidraw1 -c QPIRI -q 192.168.1.102 --mqttuser sonoff --mqttpass sonoff --tag inver2_conf -o mqtt")
-        time.sleep(0.5)
-        print("PUSH Another Done")
 
-        counter_another_status = 0
-    else:
-        counter_another_status += 1
+#         print("PUSH Another Done")
 
-    # print("SLEEP 20s")
-    # time.sleep(20)
+#         counter_another_status = 0
+#     else:
+#         counter_another_status += 1
+
+#     # print("SLEEP 20s")
+#     # time.sleep(20)
 
