7f39ee6fb902db7df2c31818a4b6b6e1a559a299
azidanit <azidan.it@gmail.com>
2022-05-01 09:24:02 +0700

    rev command to send twice

--- command_loop.py
+++ command_loop.py
@@ -22,70 +22,76 @@ def on_message(client, userdata, message):
 
     if message.topic == "homeassistant/sensor/inverter_3":
         print("EXEC inv3")
-        if message.payload.decode() == "POP00":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP00")
-        if message.payload.decode() == "POP01":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP01")
-        if message.payload.decode() == "POP02":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP02")
+        # if message.payload.decode() == "POP00":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP00")
+        # if message.payload.decode() == "POP01":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP01")
+        # if message.payload.decode() == "POP02":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c POP02")
         
-        if message.payload.decode() == "PCP00":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP00")
-        if message.payload.decode() == "PCP01":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP01")
-        if message.payload.decode() == "PCP02":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP02")
-        if message.payload.decode() == "PCP03":
-            os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP03")
+        # if message.payload.decode() == "PCP00":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP00")
+        # if message.payload.decode() == "PCP01":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP01")
+        # if message.payload.decode() == "PCP02":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP02")
+        # if message.payload.decode() == "PCP03":
+        #     os.system("mpp-solar -p /dev/ttyUSB0 -P PI30 -c PCP03")
         
         command_str = message.payload.decode()
         full_str_cmd = "mpp-solar -p /dev/ttyUSB0 -P PI30 -c " + command_str
         os.system(full_str_cmd)
+        time.sleep(0.1)
+        os.system(full_str_cmd)
     
     if message.topic == "homeassistant/sensor/inverter_1":
         print("EXEC inv1")
-        if message.payload.decode() == "POP00":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP00")
-        if message.payload.decode() == "POP01":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP01")
-        if message.payload.decode() == "POP02":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP02")
+        # if message.payload.decode() == "POP00":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP00")
+        # if message.payload.decode() == "POP01":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP01")
+        # if message.payload.decode() == "POP02":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c POP02")
         
-        if message.payload.decode() == "PCP00":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP00")
-        if message.payload.decode() == "PCP01":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP01")
-        if message.payload.decode() == "PCP02":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP02")
-        if message.payload.decode() == "PCP03":
-            os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP03")
+        # if message.payload.decode() == "PCP00":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP00")
+        # if message.payload.decode() == "PCP01":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP01")
+        # if message.payload.decode() == "PCP02":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP02")
+        # if message.payload.decode() == "PCP03":
+        #     os.system("mpp-solar -p /dev/hidraw0 -P PI30 -c PCP03")
 
         command_str = message.payload.decode()
         full_str_cmd = "mpp-solar -p /dev/hidraw0 -P PI30 -c " + command_str
+        os.system(full_str_cmd)
+        time.sleep(0.1)
         os.system(full_str_cmd)
     
 
     if message.topic == "homeassistant/sensor/inverter_2":
         print("EXEC inv2")
 
-        if message.payload.decode() == "POP00":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP00")
-        if message.payload.decode() == "POP01":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP01")
-        if message.payload.decode() == "POP02":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP02")
+        # if message.payload.decode() == "POP00":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP00")
+        # if message.payload.decode() == "POP01":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP01")
+        # if message.payload.decode() == "POP02":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c POP02")
         
-        if message.payload.decode() == "PCP00":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP00")
-        if message.payload.decode() == "PCP01":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP01")
-        if message.payload.decode() == "PCP02":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP02")
-        if message.payload.decode() == "PCP03":
-            os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP03")
+        # if message.payload.decode() == "PCP00":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP00")
+        # if message.payload.decode() == "PCP01":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP01")
+        # if message.payload.decode() == "PCP02":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP02")
+        # if message.payload.decode() == "PCP03":
+        #     os.system("mpp-solar -p /dev/hidraw1 -P PI30 -c PCP03")
 
         command_str = message.payload.decode()
         full_str_cmd = "mpp-solar -p /dev/hidraw1 -P PI30 -c " + command_str
+        os.system(full_str_cmd)
+        time.sleep(0.1)
         os.system(full_str_cmd)
 
   
